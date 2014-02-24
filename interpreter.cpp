d58f8d6f2c1a09a30b3c499b051b9a1246a3a333
Michael Burge <michael.burge@rentrakmail.com>
2014-02-24 04:24:44 -0800

    Many stubs for the XBoard protocol

--- interpreter.cpp
+++ interpreter.cpp
@@ -9,13 +9,15 @@ Interpreter::Interpreter() :
     _state(StandardGameState::new_game()),
     running(true),
     difficulty(2),
-    max_nodes(1000)
+    max_nodes(1000),
+    is_running_over_xboard(false),
+    xboard_protocol_version(0)
     { }
 
 void Interpreter::prompt() {
-    cout << "Welcome to Super Happy Xiangqi funtime!" << endl;
+    cout << "Welcome to Michael Burge's Ultimate Chinese Chess Happiness!" << endl;
+    cout.setf(ios::unitbuf);
     while (this->running) {
-        cout << "> ";
         string input;
         getline(cin, input);
         dispatch_command(input);
@@ -52,7 +54,7 @@ void Interpreter::dispatch_command(const string& command) {
     case str2int("moves"):
         this->cmd_show_moves(remaining_text);
         break;
-    case str2int("computer"):
+    case str2int("run_computer"):
         this->cmd_run_computer();
         break;
     case str2int("difficulty"):
@@ -64,6 +66,97 @@ void Interpreter::dispatch_command(const string& command) {
     case str2int("move_scores"):
 	this->cmd_show_move_scores();
 	break;
+
+    // XBoard commands
+    case str2int("xboard"):
+	cmd_xboard(); break;
+    case str2int("protover"):
+	cmd_protover(remaining_text); break;
+    case str2int("accepted"):
+	cmd_accepted(); break;
+    case str2int("rejected"):
+	cmd_rejected(); break;
+    case str2int("variant"):
+	cmd_variant(remaining_text); break;
+    case str2int("quit"):
+	cmd_exit(); break;
+    case str2int("random"):
+
+	cmd_random(); break;
+    case str2int("force"):
+	cmd_force(); break;
+    case str2int("go"):
+	cmd_go(); break;
+    case str2int("playother"):
+	cmd_playother(); break;
+    case str2int("white"):
+	cmd_white(); break;
+    case str2int("black"):
+	cmd_black(); break;
+    case str2int("level"):
+	cmd_level(remaining_text); break;
+    case str2int("st"):
+	cmd_st(remaining_text); break;
+    case str2int("sd"):
+	cmd_sd(remaining_text); break;
+    case str2int("nps"):
+	cmd_nps(remaining_text); break;
+    case str2int("time"):
+	cmd_time(remaining_text); break;
+    case str2int("otim"):
+	cmd_otim(remaining_text); break;
+    case str2int("usermove"):
+	cmd_usermove(remaining_text); break;
+    case str2int("?"):
+	cmd_movenow(remaining_text); break;
+    case str2int("ping"):
+	cmd_ping(remaining_text); break;
+    case str2int("draw"):
+	cmd_draw(); break;
+    case str2int("result"):
+	cmd_result(remaining_text); break;
+    case str2int("setboard"):
+	cmd_setboard(remaining_text); break;
+    case str2int("edit"):
+	cmd_edit(); break;
+    case str2int("hint"):
+	cmd_hint(); break;
+    case str2int("bk"):
+	cmd_bk(); break;
+    case str2int("undo"):
+	cmd_undo(); break;
+    case str2int("remove"):
+	cmd_remove(); break;
+    case str2int("hard"):
+	cmd_enable_pondering(); break;
+    case str2int("easy"):
+	cmd_disable_pondering(); break;
+    case str2int("post"): break;
+	cmd_post(); break;
+    case str2int("nopost"):
+	cmd_nopost(); break;
+    case str2int("analyze"):
+	cmd_analyze(); break;
+    case str2int("name"):
+	cmd_name(); break;
+    case str2int("rating"):
+	cmd_rating(); break;
+    case str2int("ics"):
+	cmd_ics(remaining_text); break;
+    case str2int("computer"):
+	cmd_set_is_computer_opponent(); break;
+    case str2int("pause"):
+	cmd_pause(); break;
+    case str2int("resume"):
+	cmd_resume(); break;
+    case str2int("memory"):
+	cmd_memory(remaining_text); break;
+    case str2int("cores"):
+	cmd_cores(remaining_text); break;
+    case str2int("egtpath"):
+	cmd_egtpath(remaining_text); break;
+    case str2int("option"):
+	cmd_option(remaining_text); break;
     default:
         this->cmd_unknown();
         break;
@@ -147,7 +240,186 @@ void Interpreter::cmd_show_move_scores() {
     print_move_scores(move_scores(this->state(), standard_score_function));
 }
 
+void Interpreter::cmd_xboard() {
+    this->is_running_over_xboard = true;
+}
+
+void Interpreter::cmd_protover(const string& remaining_text) {
+    handle_integer(this->xboard_protocol_version, "XBoard protocol version", remaining_text);
+}
+
 const StandardGameState& Interpreter::state() {
     return this->_state;
 }
 
+void Interpreter::cmd_accepted() {
+}
+
+void Interpreter::cmd_rejected() {
+}
+
+void Interpreter::cmd_variant(const string&) {
+}
+
+void Interpreter::cmd_quit() {
+}
+
+void Interpreter::cmd_random() {
+}
+
+void Interpreter::cmd_force() {
+}
+
+void Interpreter::cmd_go() {
+}
+
+void Interpreter::cmd_playother() {
+}
+
+void Interpreter::cmd_white() {
+}
+
+void Interpreter::cmd_black() {
+}
+
+void Interpreter::cmd_level(const string&) {
+}
+
+void Interpreter::cmd_st(const string&) {
+}
+
+void Interpreter::cmd_sd(const string&) {
+}
+
+void Interpreter::cmd_nps(const string&) {
+}
+
+void Interpreter::cmd_time(const string&) {
+}
+
+void Interpreter::cmd_otim(const string&) {
+}
+
+void Interpreter::cmd_usermove(const string&) {
+}
+
+void Interpreter::cmd_movenow(const string&) {
+}
+
+void Interpreter::cmd_ping(const string&) {
+}
+
+void Interpreter::cmd_draw() {
+}
+
+void Interpreter::cmd_result(const string&) {
+}
+
+void Interpreter::cmd_setboard(const string&) {
+}
+
+void Interpreter::cmd_edit() {
+}
+
+void Interpreter::cmd_hint() {
+}
+
+void Interpreter::cmd_bk() {
+}
+
+void Interpreter::cmd_undo() {
+}
+
+void Interpreter::cmd_remove() {
+}
+
+void Interpreter::cmd_enable_pondering() {
+}
+
+void Interpreter::cmd_disable_pondering() {
+}
+
+void Interpreter::cmd_post() {
+}
+
+void Interpreter::cmd_nopost() {
+}
+
+void Interpreter::cmd_analyze() {
+}
+
+void Interpreter::cmd_name() {
+}
+
+void Interpreter::cmd_rating() {
+}
+
+void Interpreter::cmd_ics(const string&) {
+}
+
+void Interpreter::cmd_set_is_computer_opponent() {
+}
+
+void Interpreter::cmd_pause() {
+}
+
+void Interpreter::cmd_resume() {
+}
+
+void Interpreter::cmd_memory(const string&) {
+}
+
+void Interpreter::cmd_cores(const string&) {
+}
+
+void Interpreter::cmd_egtpath(const string&) {
+}
+
+void Interpreter::cmd_option(const string&) {
+}
+
+    
+void Interpreter::out_feature() {
+}
+
+void Interpreter::out_illegal_move(const Move&) {
+}
+
+void Interpreter::out_move(const Move&) {
+}
+
+void Interpreter::out_result() {
+}
+
+void Interpreter::out_resign() {
+}
+
+void Interpreter::out_offer_draw() {
+}
+
+void Interpreter::out_tellopponent(const string&) {
+}
+
+void Interpreter::out_tellothers(const string&) {
+}
+
+void Interpreter::out_tellall(const string&) {
+}
+
+void Interpreter::out_telluser(const string&) {
+}
+
+void Interpreter::out_tellusererror(const string&) {
+}
+
+void Interpreter::out_askuser(const string&, const string&) {
+}
+
+void Interpreter::out_tellics(const string&) {
+}
+
+void Interpreter::out_tellicsnoalias(const string&) {
+}
+
+void Interpreter::out_comment(const string&) {
+}
